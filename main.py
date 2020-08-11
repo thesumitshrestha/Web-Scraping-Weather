be0bed2be9fefe5ec054e44b1147edc24bcbf793
thesumitshrestha <summitshrestha8@gmail.com>
2020-08-11 20:46:44 +0545

    Webscrapping Weather 2 weeks data

---
+++ main.py
@@ -0,0 +1,25 @@
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+
+page = requests.get("https://www.bbc.com/weather/2643743")
+soup = BeautifulSoup(page.content, 'html.parser')
+main_div = soup.find('div', class_='wr-day-carousel')
+# items = main_div.find_all(class_='wr-day__body')
+items = main_div.find_all(class_='wr-day')
+
+weather_descriptions = [item.find(class_='wr-day__weather-type-description').get_text() for item in items]
+# high_temperature = [item.find(class_='wr-day-temperature__high-value').get_text() for item in items]
+low_temperature = [item.find(class_='wr-day-temperature__low-value').get_text() for item in items]
+period_time = [item.find(class_='wr-day__title').get_text() for item in items]
+weather = pd.DataFrame(
+    {
+        'date': period_time,
+        'temperature': low_temperature,
+        'description': weather_descriptions
+    }
+)
+
+print(weather)
+
+weather.to_csv('weather.csv')
