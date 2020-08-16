75e546d169ef37dc07367706cee38d44e1bfab4b
thesumitshrestha <summitshrestha8@gmail.com>
2020-08-16 10:36:21 +0545

    Modified

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ main_div = soup.find('div', class_='wr-day-carousel')
 items = main_div.find_all(class_='wr-day')
 
 weather_descriptions = [item.find(class_='wr-day__weather-type-description').get_text() for item in items]
-# high_temperature = [item.find(class_='wr-day-temperature__high-value').get_text() for item in items]
+high_temperature = [item.find(class_='wr-day-temperature__high-value').get_text() for item in items]
 low_temperature = [item.find(class_='wr-day-temperature__low-value').get_text() for item in items]
 period_time = [item.find(class_='wr-day__title').get_text() for item in items]
 weather = pd.DataFrame(
     {
         'date': period_time,
+        'high': high_temperature,
         'temperature': low_temperature,
         'description': weather_descriptions
     }
